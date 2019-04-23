5e7aa51854197f0116d8fbc85452c2667c46fc0b
John Lee <jslee1992@gmail.com>
2019-04-23 00:46:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function logDriverNames(driver){
+  driver.forEach(function(driver){
+    console.log(driver.name);
+  });
+}
+
+function logDriversByHometown(driver, location){
+  driver.forEach(function(driver){
+    if (driver.hometown === location){
+      console.log(driver.name);
+    }
+  });
+}
+
+function driversByRevenue(driver){
+  return driver.slice().sort(function(a, b){
+    return a.revenue - b.revenue;
+  });
+}
+
+function driversByName(driver){
+  return driver.slice().sort(function(a,b){
+    return a.name.localeCompare(b.name)
+  })
+}
+
+function totalRevenue(driver){
+  return driver.reduce(function(acc, curr){
+    return acc + curr.revenue;
+  },0)
+}
+
+function averageRevenue(driver){
+  return totalRevenue(driver)/driver.length
+}
